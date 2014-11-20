72f36d57b55397f64d0e35263061b5f219453215
Kevin Luo <kevin.luo@siloon.com>
2014-11-20 17:48:52 +0800

    codes clean

--- ui/bar-chart.reel/bar-chart.js
+++ ui/bar-chart.reel/bar-chart.js
@@ -2,223 +2,212 @@
  * @module ui/bar-chart.reel
  * @requires montage/ui/component
  */
-var Chartjs = require("../chartjs.reel").Chartjs;
+var Chartjs = require( "../chartjs.reel" ).Chartjs;
 
 /**
  * @class BarChart
  * @extends Component
  */
-exports.BarChart = Chartjs.specialize(/** @lends BarChart# */ {
-    constructor: {
-        value: function BarChart() {
-            this.super();
-            this.selfRefesh = false;
-        }
-    },
-    options: {
-        value: {
-            //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-            scaleBeginAtZero: true,
+exports.BarChart = Chartjs.specialize( /** @lends BarChart# */ {
+	constructor: {
+		value: function BarChart() {
+			this.super();
+			this.selfRefesh = false;
+		}
+	},
+	options: {
+		value: {
+			//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+			scaleBeginAtZero: true,
 
-            //Boolean - Whether grid lines are shown across the chart
-            scaleShowGridLines: true,
+			//Boolean - Whether grid lines are shown across the chart
+			scaleShowGridLines: true,
 
-            //String - Colour of the grid lines
-            scaleGridLineColor: "rgba(0,0,0,.05)",
+			//String - Colour of the grid lines
+			scaleGridLineColor: "rgba(0,0,0,.05)",
 
-            //Number - Width of the grid lines
-            scaleGridLineWidth: 1,
+			//Number - Width of the grid lines
+			scaleGridLineWidth: 1,
 
-            //Boolean - If there is a stroke on each realPic
-            barShowStroke: true,
+			//Boolean - If there is a stroke on each realPic
+			barShowStroke: true,
 
-            //Number - Pixel width of the realPic stroke
-            barStrokeWidth: 2,
+			//Number - Pixel width of the realPic stroke
+			barStrokeWidth: 2,
 
-            //Number - Spacing between each of the X value sets
-            barValueSpacing: 5,
+			//Number - Spacing between each of the X value sets
+			barValueSpacing: 5,
 
-            //Number - Spacing between data sets within X values
-            barDatasetSpacing: 1,
-            responsive: true,
-            //String - A legend template
-            legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-        }
-    },
-    setSeriesFillColor: {
-        value: function (serieIdx, fillClr) {
-            if (this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length) {
-                this.realPic.datasets[serieIdx].fillColor = fillClr;
-                this.datasets[serieIdx].fillColor = fillClr;
-                if (this.realPic.datasets[serieIdx].bars) {
-                    for (var k = 0; k < this.realPic.datasets[serieIdx].bars.length; k++) {
-                        this.realPic.datasets[serieIdx].bars[k].fillColor = fillClr;
-                    }
-                }
-                this.needsDraw = true;
-            }
-            else {
-                throw "Your index is out of the array length.";
-            }
-        }
-    },
-    getSeriesFillColor: {
-        value: function (serieIdx) {
-            if (this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length) {
-                return  this.realPic.datasets[serieIdx].fillColor;
-            }
-            else {
-                throw "Your index is out of the array length.";
-            }
-        }
-    },
-    setSeriesStrokeColor: {
-        value: function (serieIdx, strokeClr) {
-            if (this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length) {
-                this.realPic.datasets[serieIdx].strokeColor = strokeClr;
-                this.datasets[serieIdx].strokeColor = strokeClr;
-                if (this.realPic.datasets[serieIdx].bars) {
-                    for (var k = 0; k < this.realPic.datasets[serieIdx].bars.length; k++) {
-                        this.realPic.datasets[serieIdx].bars[k].strokeColor = strokeClr;
-                    }
-                }
-                this.needsDraw = true;
-            }
-            else {
-                throw "Your serial index is out of the array length.";
-            }
-        }
-    },
-    getSeriesStrokeColor: {
-        value: function (serieIdx) {
-            if (this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length) {
-                return  this.realPic.datasets[serieIdx].strokeColor;
-            }
-            else {
-                throw "Your serial index is out of the array length.";
-            }
-        }
-    },
-    setLabels: {
-        value: function (dataidx, label) {
-//            if (this.realPic && this.realPic.datasets && this.realPic.datasets[0].bars && dataidx < this.realPic.datasets[0].bars.length) {
-//                for (var s = 0; s < this.realPic.datasets.length; s++) {
-//                    this.realPic.datasets[s].label = label;
-//                    if (this.realPic.datasets[s].bars) {
-//                        this.realPic.datasets[s].bars[dataidx].label = label;
-//                        this.needsDraw = true;
-//                    }
-//                }
-//            }
-            if (this.labels && dataidx < this.labels.length) {
-                this.labels[dataidx] = label;
-                this.selfRefesh = true;
-                this.needsDraw = true;
-            }
-            else {
-                throw "Your label index is out of the array length.";
-            }
-        }
-    },
-    getLabels: {
-        value: function (dataidx) {
-//            if (this.realPic && this.realPic.datasets && this.realPic.datasets[0].bars && dataidx < this.realPic.datasets[0].bars.length) {
-//                return  this.realPic.datasets[0].bars[dataidx].label;
-            if (this.labels && dataidx < this.labels.length) {
-                return this.labels[dataidx];
-            }
-            else {
-                throw "Your label index is out of the array length.";
-            }
-        }
-    },
-    setSeriesData: {
-        value: function (serieIdx, dataidx, val) {
-            if (this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length) {
-                if (this.realPic.datasets[serieIdx].bars && dataidx < this.realPic.datasets[serieIdx].bars.length) {
-                    this.datasets[serieIdx].data[dataidx] = val;
-                    this.realPic.datasets[serieIdx].bars[dataidx].value = val;
-                    this.needsDraw = true;
-                }
-                else {
-                    throw "Your data index is out of the array length.";
-                }
-            }
-            else {
-                throw "Your data index is out of the array length.";
-            }
-        }
-    },
-    getSeriesData: {
-        value: function (serieIdx, dataidx) {
-            if (this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length) {
-                if (this.realPic.datasets[serieIdx].bars && dataidx < this.realPic.datasets[serieIdx].bars.length) {
-                    return this.realPic.datasets[serieIdx].bars[dataidx].value;
-                }
-                else {
-                    throw "Your data index is out of the array length.";
-                }
-            }
-            else {
-                throw "Your data index is out of the array length.";
-            }
-        }
-    },
-    destroy: {
-        value: function () {
-            this.realPic.destroy();
-            this.chart.destroy();
-            this.datasets = null;
-            this.allData = null;
-            this.labels = null;
-            this.chart = null;
-            this.realPic = null;
-        }
-    },
-    setFreshData: {
-        value: function (datasets, labels) {
-            this.datasets = datasets;
-            this.labels = labels;
-            this.selfRefesh = true;
-            this.needsDraw = true;
-        }
-    },
-    getDatasets: {
-        value: function () {
+			//Number - Spacing between data sets within X values
+			barDatasetSpacing: 1,
+			responsive: true,
+			//String - A legend template
+			legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+		}
+	},
+	setSeriesFillColor: {
+		value: function( serieIdx, fillClr ) {
+			if ( this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length ) {
+				this.realPic.datasets[serieIdx].fillColor = fillClr;
+				this.datasets[serieIdx].fillColor = fillClr;
+				if ( this.realPic.datasets[serieIdx].bars ) {
+					for ( var k = 0; k < this.realPic.datasets[serieIdx].bars.length; k++ ) {
+						this.realPic.datasets[serieIdx].bars[k].fillColor = fillClr;
+					}
+				}
+				this.needsDraw = true;
+			}
+			else {
+				throw "Your index is out of the array length.";
+			}
+		}
+	},
+	getSeriesFillColor: {
+		value: function( serieIdx ) {
+			if ( this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length ) {
+				return  this.realPic.datasets[serieIdx].fillColor;
+			}
+			else {
+				throw "Your index is out of the array length.";
+			}
+		}
+	},
+	setSeriesStrokeColor: {
+		value: function( serieIdx, strokeClr ) {
+			if ( this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length ) {
+				this.realPic.datasets[serieIdx].strokeColor = strokeClr;
+				this.datasets[serieIdx].strokeColor = strokeClr;
+				if ( this.realPic.datasets[serieIdx].bars ) {
+					for ( var k = 0; k < this.realPic.datasets[serieIdx].bars.length; k++ ) {
+						this.realPic.datasets[serieIdx].bars[k].strokeColor = strokeClr;
+					}
+				}
+				this.needsDraw = true;
+			}
+			else {
+				throw "Your serial index is out of the array length.";
+			}
+		}
+	},
+	getSeriesStrokeColor: {
+		value: function( serieIdx ) {
+			if ( this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length ) {
+				return  this.realPic.datasets[serieIdx].strokeColor;
+			}
+			else {
+				throw "Your serial index is out of the array length.";
+			}
+		}
+	},
+	setLabels: {
+		value: function( dataidx, label ) {
+			if ( this.labels && dataidx < this.labels.length ) {
+				this.labels[dataidx] = label;
+				this.selfRefesh = true;
+				this.needsDraw = true;
+			}
+			else {
+				throw "Your label index is out of the array length.";
+			}
+		}
+	},
+	getLabels: {
+		value: function( dataidx ) {
+			if ( this.labels && dataidx < this.labels.length ) {
+				return this.labels[dataidx];
+			}
+			else {
+				throw "Your label index is out of the array length.";
+			}
+		}
+	},
+	setSeriesData: {
+		value: function( serieIdx, dataidx, val ) {
+			if ( this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length ) {
+				if ( this.realPic.datasets[serieIdx].bars && dataidx < this.realPic.datasets[serieIdx].bars.length ) {
+					this.datasets[serieIdx].data[dataidx] = val;
+					this.realPic.datasets[serieIdx].bars[dataidx].value = val;
+					this.needsDraw = true;
+				}
+				else {
+					throw "Your data index is out of the array length.";
+				}
+			}
+			else {
+				throw "Your data index is out of the array length.";
+			}
+		}
+	},
+	getSeriesData: {
+		value: function( serieIdx, dataidx ) {
+			if ( this.realPic && this.realPic.datasets && serieIdx < this.realPic.datasets.length ) {
+				if ( this.realPic.datasets[serieIdx].bars && dataidx < this.realPic.datasets[serieIdx].bars.length ) {
+					return this.realPic.datasets[serieIdx].bars[dataidx].value;
+				}
+				else {
+					throw "Your data index is out of the array length.";
+				}
+			}
+			else {
+				throw "Your data index is out of the array length.";
+			}
+		}
+	},
+	destroy: {
+		value: function() {
+			this.realPic.destroy();
+			this.chart.destroy();
+			this.datasets = null;
+			this.allData = null;
+			this.labels = null;
+			this.chart = null;
+			this.realPic = null;
+		}
+	},
+	setFreshData: {
+		value: function( datasets, labels ) {
+			this.datasets = datasets;
+			this.labels = labels;
+			this.selfRefesh = true;
+			this.needsDraw = true;
+		}
+	},
+	getDatasets: {
+		value: function() {
 
-            var dataset = null;
-            dataset = {labels: [], datasets: []};
-            dataset.labels = this.labels;
-            dataset.datasets = this.datasets;
-            return dataset;
-        }
-    },
-    removeAllData: {
-        value: function () {
-            if (this.realPic && this.realPic.datasets)
-                for (var k = 0; k < this.realPic.datasets.length; k++) {
-                    this.realPic.removeData();
-                }
-        }
-    },
-    drawChart: {
-        value: function () {
-            if (this.chartContext == null) return;
-            this.allData = this.getDatasets();
-            if (!this.allData.labels || !this.allData.datasets) return;
-            if (!this.realPic) {
-                this.chart = new Chart(this.chartContext);
-                this.realPic = this.chart.Bar(this.allData, this.options);
-            }
-            else if (this.selfRefesh) {
-                this.realPic.destroy();
-                this.realPic = this.chart.Bar(this.allData, this.options);
-                this.selfRefesh = false;
-            }
-            else {
-                this.realPic.update();
-            }
-        }
-    }
+			var dataset = null;
+			dataset = {labels: [], datasets: []};
+			dataset.labels = this.labels;
+			dataset.datasets = this.datasets;
+			return dataset;
+		}
+	},
+	removeAllData: {
+		value: function() {
+			if ( this.realPic && this.realPic.datasets )
+				for ( var k = 0; k < this.realPic.datasets.length; k++ ) {
+					this.realPic.removeData();
+				}
+		}
+	},
+	drawChart: {
+		value: function() {
+			if ( this.chartContext == null ) return;
+			this.allData = this.getDatasets();
+			if ( !this.allData.labels || !this.allData.datasets ) return;
+			if ( !this.realPic ) {
+				this.chart = new Chart( this.chartContext );
+				this.realPic = this.chart.Bar( this.allData, this.options );
+			}
+			else if ( this.selfRefesh ) {
+				this.realPic.destroy();
+				this.realPic = this.chart.Bar( this.allData, this.options );
+				this.selfRefesh = false;
+			}
+			else {
+				this.realPic.update();
+			}
+		}
+	}
 
-});
+} );
--- ui/chartjs.reel/chartjs.js
+++ ui/chartjs.reel/chartjs.js
@@ -2,110 +2,103 @@
  * @module ui/chartjs.reel
  * @requires montage/ui/component
  */
-var Component = require("montage/ui/component").Component;
-var Chart = require("../../core/chart");
+var Component = require( "montage/ui/component" ).Component;
+var Chart = require( "../../core/chart" );
 
 /**
  * @class Chartjs
  * @extends Component
  */
-exports.Chartjs = Component.specialize(/** @lends Chartjs# */ {
-    chartContext: {
-        value: null
-    },
-    chart: {
-        value: null
-    },
-    options: {
-        value: null
-    },
-    labels: {
-        value: null
-    },
-    datasets: {
-        value: null
-    },
-    constructor: {
-        value: function Chart() {
-            this.super();
-        }
-    },
-    getDatasets: {
-        value: null
-    },
-    enterDocument: {
-        value: function (isFirstTime) {
-            this.super(isFirstTime);
-            this.chartContext = this.element.getContext("2d");
-//			this.drawChart();
-//            this.addOwnPropertyChangeListener('labels', this);//, 'updateChart');
-            this.addRangeAtPathChangeListener('labels', this, 'updateChart');
-            this.addRangeAtPathChangeListener('datasets', this, 'updateChart');
-        }
-    },
-//    handlePropertyChange:{
-//      value:function(){
-//          debugger
-//          this.needsDraw = true;
-//      }
-//    },
-    setSeriesFillColor: {
-        value: null
-    },
-    getSeriesFillColor: {
-        value: null
-    },
-    setSeriesStrokeColor: {
-        value: null
-    },
-    getSeriesStrokeColor: {
-        value: null
-    },
-    setSeriesLabels: {
-        value: null
-    },
-    getSeriesLabels: {
-        value: null
-    },
-    setSeriesData: {
-        value: null
-    },
-    getSeriesData: {
-        value: null
-    },
-    setFreshData: {
-        value: null
-    },
-    removeAllData: {
-        value: null
-    },
-    selfRefesh: {
-        value: false
-    },
-    realPic: {
-        value: null
-    },
-    allData: {
-        value: null
-    },
-    draw: {
-        value: function () {
-            this.drawChart();
-        }
-    },
-    destroy:{
-        value:null
-    },
-    updateChart: {
-        value: function () {
-            this.needsDraw = true;
-        }
-    },
-    drawChart: {
-        value: function () {
+exports.Chartjs = Component.specialize( /** @lends Chartjs# */ {
+	chartContext: {
+		value: null
+	},
+	chart: {
+		value: null
+	},
+	options: {
+		value: null
+	},
+	labels: {
+		value: null
+	},
+	datasets: {
+		value: null
+	},
+	constructor: {
+		value: function Chart() {
+			this.super();
+		}
+	},
+	getDatasets: {
+		value: null
+	},
+	enterDocument: {
+		value: function( isFirstTime ) {
+			this.super( isFirstTime );
+			this.chartContext = this.element.getContext( "2d" );
+			this.addRangeAtPathChangeListener( 'labels', this, 'updateChart' );
+			this.addRangeAtPathChangeListener( 'datasets', this, 'updateChart' );
+		}
+	},
 
-        }
-    }
+	setSeriesFillColor: {
+		value: null
+	},
+	getSeriesFillColor: {
+		value: null
+	},
+	setSeriesStrokeColor: {
+		value: null
+	},
+	getSeriesStrokeColor: {
+		value: null
+	},
+	setSeriesLabels: {
+		value: null
+	},
+	getSeriesLabels: {
+		value: null
+	},
+	setSeriesData: {
+		value: null
+	},
+	getSeriesData: {
+		value: null
+	},
+	setFreshData: {
+		value: null
+	},
+	removeAllData: {
+		value: null
+	},
+	selfRefesh: {
+		value: false
+	},
+	realPic: {
+		value: null
+	},
+	allData: {
+		value: null
+	},
+	draw: {
+		value: function() {
+			this.drawChart();
+		}
+	},
+	destroy: {
+		value: null
+	},
+	updateChart: {
+		value: function() {
+			this.needsDraw = true;
+		}
+	},
+	drawChart: {
+		value: function() {
+
+		}
+	}
 
 
-});
+} );
