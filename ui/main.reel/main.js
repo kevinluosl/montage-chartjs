51841011fd31d65dfc447b8f103adbd1fce0801f
Kevin Luo <kevin.luo@siloon.com>
2014-11-27 11:36:47 +0800

    test codes

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -184,10 +184,21 @@ exports.Main = Component.specialize( /** @lends Main# */ {
 //                self.barDatasets[0].fillColor="rgba(100,220,220,0.5)";
 //                self.templateObjects.barChart.reDraw();
 //				self.barDatasets[0].label = 'Meg';
-//
-				self.templateObjects.chartRangeController.content[0].label = 'New Label Test';
 
-			}, 5000 );
+//				self.templateObjects.chartRangeController.labels[0] = 'New Label Test';
+
+				self.templateObjects.chartRangeController.content[0] = {
+					label: "aaaaaaa",
+					fillColor: "rgba(220,220,220,0.2)",
+					strokeColor: "rgba(220,220,220,1)",
+					pointColor: "rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "rgba(220,220,220,1)",
+					data: [11, 11, 11, 11, 11, 55, 11]
+				}
+
+			}, 3000 );
 
 		}
 	}
