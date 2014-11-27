f14067e85d013b5f610e02c243d0e501bcb78db6
Kevin Luo <kevin.luo@siloon.com>
2014-11-27 09:49:48 +0800

    codes clean in chart-ranger

--- ui/chart-controller.reel/chart-controller.js
+++ ui/chart-controller.reel/chart-controller.js
@@ -2,33 +2,21 @@
  * @module ui/chart-controller.reel
  * @requires montage/ui/component
  */
-var RangeController = require("montage/core/range-controller").RangeController;
+var RangeController = require( "montage/core/range-controller" ).RangeController;
 
 /**
  * @class ChartController
  * @extends Component
  */
-exports.ChartController = RangeController.specialize(/** @lends ChartController# */ {
-    constructor: {
-        value: function ChartController(content, labels) {
-            this.super(content);
-            this.labels = labels;
-            //this.addOwnPropertyChangeListener("content",this);
-            //this.addPathChangeListener("content",this,"handleValuesChange");
-            //this.addPathChangeListener("content",this,"handleValuesChange");
-        }
-    },
-    //handleValuesChange:{
-    //    value: function(plus,minus,index){
-    //        debugger
-    //    }
-    //},
-    //handlePropertyChange:{
-    //    value:function(){
-    //        debugger
-    //    }
-    //},
-    labels: {
-        value: null
-    }
-});
+exports.ChartController = RangeController.specialize( /** @lends ChartController# */ {
+	constructor: {
+		value: function ChartController( content, labels ) {
+			this.super( content );
+			this.labels = labels;
+
+		}
+	},
+	labels: {
+		value: null
+	}
+} );
